34a03cb150d18de1a13bdffb2735ab49a7fa0418
Indian-Jones <bsarabeswaran@gmail.com>
2021-09-06 16:59:19 -0400

    Initial files

---
+++ mvsc.lua
@@ -0,0 +1,116 @@
+assert(rb,"Run fbneo-training-mode.lua")
+
+p1maxhealth = 0x90
+p2maxhealth = 0x90
+p1maxmeter = 0x03
+p2maxmeter = 0x03
+
+local p1health = 0xFF3271
+local p2health = 0xFF3671
+
+local p1meter = 0xFF3274
+local p2meter = 0xFF3674
+
+translationtable = {
+	"left",
+	"right",
+	"up",
+	"down",
+	"button1",
+	"button2",
+	"button3",
+	"button4",
+	"button5",
+	"button6",
+	"coin",
+	"start",
+	["Left"] = 1,
+	["Right"] = 2,
+	["Up"] = 3,
+	["Down"] = 4,
+	["Weak Punch"] = 5,
+	["Medium Punch"] = 6,
+	["Strong Punch"] = 7,
+	["Weak Kick"] = 8,
+	["Medium Kick"] = 9,
+	["Strong Kick"] = 10,
+	["Coin"] = 11,
+	["Start"] = 12,
+}
+
+gamedefaultconfig = {
+	hud = {
+		combotextx=180,
+		combotexty=45,
+		comboenabled=true,
+		p1healthx=32,
+		p1healthy=21,
+		p1healthenabled=true,
+		p2healthx=341,
+		p2healthy=21,
+		p2healthenabled=true,
+		p1meterx=176,
+		p1metery=215,
+		p1meterenabled=true,
+		p2meterx=205,
+		p2metery=215,
+		p2meterenabled=true,
+	},
+}
+
+function playerOneFacingLeft()
+	return rb(0xff344b)==1
+end
+	
+function playerTwoFacingLeft()
+	return rb(0xff344b)==0
+end
+
+function playerOneInHitstun()
+	return rb(0xff3520)~=0
+end
+
+function playerTwoInHitstun()
+	return rb(0xff3120)~=0
+end
+
+function readPlayerOneHealth()
+	return rb(p1health)
+end
+
+function readPlayerTwoHealth()
+	return rb(p2health)
+end
+
+function writePlayerOneHealth(health)
+	wb(p1health, health)
+end
+
+function writePlayerTwoHealth(health)
+	wb(p2health, health)
+end
+
+function readPlayerOneMeter()
+	return rb(p1meter)
+end
+
+function writePlayerOneMeter(meter)
+	wb(p1meter, meter)
+end
+
+function readPlayerTwoMeter()
+	return rb(p2meter)
+end
+
+function writePlayerTwoMeter(meter)
+	wb(p2meter, meter)
+end
+
+local infiniteTime = function()
+	memory.writebyte(0xFF4008, 0x99)
+end
+
+function Run()
+	infiniteTime()
+end
+
